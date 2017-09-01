bf3894c7f190685a90336a6f13f1e510a296606b
vmzakharov <zakharov.vladimir.m@gmail.com>
2017-08-31 23:53:33 -0400

    Expanded color space for Black and Green

--- maze-parser/src/main/java/nvv/robotics/image/ColorConstants.java
+++ maze-parser/src/main/java/nvv/robotics/image/ColorConstants.java
@@ -3,10 +3,10 @@ package nvv.robotics.image;
 public class ColorConstants
 {
     public static final double S_MAX  = 1.00;
-    public static final double S_MIN  = 0.32; // 0.34;
+    public static final double S_MIN  = 0.318;
 
     public static final double L_MAX  = 0.80;
-    public static final double L_MIN  = 0.16;
+    public static final double L_MIN  = 0.10;
 
     public static final double S_RANGE_MID = (S_MAX + S_MIN)/2.0;
     public static final double L_RANGE_MID = (L_MAX + L_MIN)/2.0;
