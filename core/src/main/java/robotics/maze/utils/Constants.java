cb4ec33400123b01cb54a16481406a21b2e1f6e7
Nikhil Nanivadekar <nikhil.nanivadekar@gs.com>
2017-09-17 03:30:38 +0100

    Fix track width.

--- core/src/main/java/robotics/maze/utils/Constants.java
+++ core/src/main/java/robotics/maze/utils/Constants.java
@@ -4,7 +4,7 @@ public class Constants
 {
     public static final String EV3_IP_ADDRESS = "10.0.1.1";
     public static final double WHEEL_DIAMETER = 30.0;
-    public static final double TRACK_WIDTH = 144.0;
+    public static final double TRACK_WIDTH = 125.0;
     public static final double ROTATE_SPEED = 100.0;
     public static final double TRAVEL_SPEED = 200.0;
     public static final int ANTICLOCKWISE_ROTATION_ADJUSTMENT = 10;
